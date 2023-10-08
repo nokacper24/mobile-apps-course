0743265be78b08fb3edc95f7e5b0084924f9b9dc
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2023-10-08 17:30:06 +0200

    start work on meals app

---
+++ meals/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:meals/meals_app.dart';
+
+void main() {
+  runApp(const MealsApp());
+}
+
---
+++ meals/lib/meals_app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:meals/screens/categories_screen.dart';
+
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 131, 57, 0),
+  ),
+  textTheme: GoogleFonts.latoTextTheme(),
+);
+
+class MealsApp extends StatelessWidget {
+  const MealsApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme,
+      home: const CategoriesScreen(),
+    );
+  }
+}
